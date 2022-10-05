c863e0aa37169bfb3d1cc825ad6d81a8f5895d03
David Nevado <david.nevado@iohk.io>
2022-10-05 20:06:11 +0200

    Add `AddVo`
    Copy of MulVO, just for addition

--- src/tests.rs
+++ src/tests.rs
@@ -16,6 +16,7 @@ mod test {
 
     use crate::concrete_oracle::VerifierConcreteOracle;
     use crate::rng::SimpleHashFiatShamirRng;
+    use crate::vo::precompiled::add::AddVO;
     use crate::PIL;
     use blake2::Blake2s;
 
@@ -131,5 +132,103 @@ mod test {
 
         assert_eq!(res, ());
 
+    }
+
+    #[test]
+    fn test_simple_add() {
+        let max_degree = 17;
+        let mut rng = test_rng();
+
+        let srs = PilInstance::universal_setup(max_degree, &mut rng).unwrap();
+
+        let (pk, vk) = PilInstance::prepare_keys(&srs).unwrap();
+
+        let domain_size = 16;
+        let poly_degree = domain_size - 1;
+        let domain = GeneralEvaluationDomain::<F>::new(domain_size).unwrap();
+
+        let a_poly = DensePolynomial::<F>::rand(poly_degree, &mut rng);
+        let b_poly = DensePolynomial::<F>::rand(poly_degree, &mut rng);
+
+        let a_evals = domain.fft(&a_poly);
+        let b_evals = domain.fft(&b_poly);
+
+        let c_evals = a_evals
+            .iter()
+            .zip(b_evals.iter())
+            .map(|(&a, &b)| a + b)
+            .collect::<Vec<_>>();
+
+        let c_poly =
+            DensePolynomial::from_coefficients_slice(&domain.ifft(&c_evals));
+
+        for elem in domain.elements() {
+            assert_eq!(
+                a_poly.evaluate(&elem) + b_poly.evaluate(&elem)
+                    - c_poly.evaluate(&elem),
+                F::zero()
+            );
+        }
+
+        let a = ProverConcreteOracle {
+            label: "a".to_string(),
+            poly: a_poly,
+            evals_at_coset_of_extended_domain: None,
+            oracle_type: OracleType::Witness,
+            queried_rotations: BTreeSet::new(),
+            should_mask: true,
+        };
+
+        let b = ProverConcreteOracle {
+            label: "b".to_string(),
+            poly: b_poly,
+            evals_at_coset_of_extended_domain: None,
+            oracle_type: OracleType::Witness,
+            queried_rotations: BTreeSet::new(),
+            should_mask: true,
+        };
+
+        let c = ProverConcreteOracle {
+            label: "c".to_string(),
+            poly: c_poly,
+            evals_at_coset_of_extended_domain: None,
+            oracle_type: OracleType::Instance,
+            queried_rotations: BTreeSet::new(),
+            should_mask: false,
+        };
+
+        let mut add_vo = AddVO::<F>::new();
+        add_vo.configure(&[0, 1], &[0]);
+
+        let concrete_oracles = [a, b, c.clone()];
+
+        let vos: Vec<Box<dyn LinearisableVirtualOracle<F>>> =
+            vec![Box::new(add_vo)];
+
+        let proof = PilInstance::prove(
+            &pk,
+            &concrete_oracles,
+            &vos,
+            domain_size,
+            &domain.vanishing_polynomial().into(),
+            &mut rng,
+        )
+        .unwrap();
+
+        let a_verifier = VerifierConcreteOracle::new("a".to_string(), true);
+        let b_verifier = VerifierConcreteOracle::new("b".to_string(), true);
+
+        let _ = PilInstance::verify(
+            &vk,
+            proof,
+            &mut [a_verifier, b_verifier],
+            &mut [c],
+            &vos,
+            domain_size,
+            &domain.vanishing_polynomial().into(),
+            pk.committer_key.max_degree(),
+            &mut rng,
+        )
+        .unwrap();
     }
 }
---
+++ src/vo/precompiled/add.rs
@@ -0,0 +1,145 @@
+use ark_ff::PrimeField;
+
+use crate::{
+    concrete_oracle::OracleType,
+    vo::{
+        expression::Expression,
+        linearisation::{LinearisationOracleQuery, LinearisationQueryContext},
+        query::{InstanceQuery, Rotation, VirtualQuery, WitnessQuery},
+        LinearisableVirtualOracle, VirtualOracle,
+    },
+};
+
+pub struct AddVO<F: PrimeField> {
+    virtual_queries: [VirtualQuery; 3],
+    witness_indices: Option<Vec<usize>>,
+    instance_indices: Option<Vec<usize>>,
+    wtns_queries: Vec<WitnessQuery>,
+    instance_queries: Vec<InstanceQuery>,
+    expression: Option<Expression<F>>,
+    linearisation_expression: Option<Expression<F>>,
+}
+
+impl<F: PrimeField> AddVO<F> {
+    pub fn new() -> Self {
+        let q1 = VirtualQuery {
+            index: 0,
+            rotation: Rotation::curr(),
+            oracle_type: OracleType::Witness,
+        };
+
+        let q2 = VirtualQuery {
+            index: 1,
+            rotation: Rotation::curr(),
+            oracle_type: OracleType::Witness,
+        };
+
+        let q3 = VirtualQuery {
+            index: 0,
+            rotation: Rotation::curr(),
+            oracle_type: OracleType::Instance,
+        };
+        let virtual_queries = [q1, q2, q3];
+
+        Self {
+            virtual_queries,
+            witness_indices: None,
+            instance_indices: None,
+            wtns_queries: vec![],
+            instance_queries: vec![],
+            expression: None,
+            linearisation_expression: None,
+        }
+    }
+
+    // TODO: consider abstracting
+    pub fn configure(
+        &mut self,
+        witness_indices: &[usize; 2],
+        instance_indices: &[usize; 1],
+    ) {
+        self.witness_indices = Some(vec![]);
+        self.instance_indices = Some(vec![]);
+        for vq in &self.virtual_queries {
+            match vq.oracle_type {
+                OracleType::Witness => self.wtns_queries.push(WitnessQuery {
+                    index: witness_indices[vq.index],
+                    rotation: vq.rotation.clone(),
+                }),
+                OracleType::Instance => {
+                    self.instance_queries.push(InstanceQuery {
+                        index: instance_indices[vq.index],
+                        rotation: vq.rotation.clone(),
+                    })
+                }
+            }
+        }
+
+        let add_expression = || {
+            let a: Expression<F> = self.wtns_queries[0].into();
+            let b: Expression<F> = self.wtns_queries[1].into();
+            let c: Expression<F> = self.instance_queries[0].into();
+
+            a + b - c
+        };
+
+        // For now we keep linearisation same as without
+        let linearisation_expression = || {
+            let a: Expression<F> = LinearisationOracleQuery {
+                index: self.wtns_queries[0].index,
+                rotation: self.wtns_queries[0].rotation,
+                oracle_type: OracleType::Witness,
+                ctx: LinearisationQueryContext::AsPoly,
+            }
+            .into();
+
+            let b: Expression<F> = LinearisationOracleQuery {
+                index: self.wtns_queries[1].index,
+                rotation: self.wtns_queries[1].rotation,
+                oracle_type: OracleType::Witness,
+                ctx: LinearisationQueryContext::AsPoly,
+            }
+            .into();
+
+            let c: Expression<F> = LinearisationOracleQuery {
+                index: self.instance_queries[0].index,
+                rotation: self.instance_queries[0].rotation,
+                oracle_type: OracleType::Instance,
+                ctx: LinearisationQueryContext::AsEval,
+            }
+            .into();
+
+            a + b - c
+        };
+
+        self.expression = Some(add_expression());
+        self.linearisation_expression = Some(linearisation_expression());
+    }
+}
+
+impl<F: PrimeField> VirtualOracle<F> for AddVO<F> {
+    fn get_wtns_queries(&self) -> &[WitnessQuery] {
+        &self.wtns_queries
+    }
+
+    fn get_instance_queries(&self) -> &[InstanceQuery] {
+        &self.instance_queries
+    }
+
+    // panics if expression is not defined before proving started
+    fn get_expression(&self) -> &Expression<F> {
+        match self.expression.as_ref() {
+            None => panic!("Expression is not defined"),
+            Some(expression) => return expression,
+        }
+    }
+}
+
+impl<F: PrimeField> LinearisableVirtualOracle<F> for AddVO<F> {
+    fn get_linearisation_expression(&self) -> &Expression<F> {
+        match self.linearisation_expression.as_ref() {
+            None => panic!("Expression is not defined"),
+            Some(expression) => return expression,
+        }
+    }
+}
