ac118e1a03aa1021fd5527a43962c50ff5b3534d
David Nevado <david.nevado@iohk.io>
2022-10-12 18:54:42 +0200

    Add 4-width rescue gate VO

--- src/vo/precompiled_vos.rs
+++ src/vo/precompiled_vos.rs
@@ -133,6 +133,47 @@ impl<F: PrimeField> PrecompiledVO<F> for PrecompiledMul {
     }
 }
 
+/// Implements 4-width rescue:
+/// q_1 * w_1^5 +
+/// q_2 * w_2^5 +
+/// q_3 * w_3^5 +
+/// q_4 * w_4^5 =
+/// q_5 * w_5
+pub struct PrecompiledRescue {}
+
+impl<F> PrecompiledVO<F> for PrecompiledRescue
+where
+    F: PrimeField,
+{
+    fn get_expr_and_queries() -> (VirtualExpression<F>, Vec<VirtualQuery>) {
+        let q = (0..=4).map(|index| {
+            VirtualQuery::new(index, Rotation::curr(), OracleType::Fixed)
+        });
+        let w = (0..=4).map(|index| {
+            VirtualQuery::new(index, Rotation::curr(), OracleType::Witness)
+        });
+        let oracles = q.clone().chain(w.clone()).collect();
+
+        let rescue_expr = {
+            let q_expr: Vec<VirtualExpression<F>> =
+                q.map(|query| query.into()).collect();
+            let w_expr: Vec<VirtualExpression<F>> =
+                w.map(|query| query.into()).collect();
+            let pow_5 = |e: &VirtualExpression<F>| {
+                e.clone() * e.clone() * e.clone() * e.clone() * e.clone()
+            };
+
+            q_expr[0].clone() * pow_5(&w_expr[0])
+                + q_expr[1].clone() * pow_5(&w_expr[1])
+                + q_expr[2].clone() * pow_5(&w_expr[2])
+                + q_expr[3].clone() * pow_5(&w_expr[3])
+                - q_expr[4].clone() * w_expr[4].clone()
+        };
+
+        (rescue_expr, oracles)
+    }
+}
+
 pub struct GenericVO<F: PrimeField> {
     pub(crate) virtual_exp: VirtualExpression<F>,
     pub(crate) virtual_queries: Vec<VirtualQuery>,
@@ -213,7 +254,7 @@ mod test {
         witness::WitnessProverOracle,
     };
 
-    use super::{GenericVO, PrecompiledMul, PrecompiledVO};
+    use super::{GenericVO, PrecompiledMul, PrecompiledRescue, PrecompiledVO};
     use crate::commitment::KZG10;
     use ark_bls12_381::{Bls12_381, Fr as F};
     use ark_poly::univariate::DensePolynomial;
@@ -253,5 +294,41 @@ mod test {
         let fixed_oracles: Vec<FixedOracle<F, PC>> = vec![];
 
         mul_vo.configure(&witness_oracles, &instance_oracles, &fixed_oracles);
+    }
+
+    #[test]
+    fn test_rescue_gate() {
+        let mut rescue_vo =
+            GenericVO::<F>::init(PrecompiledRescue::get_expr_and_queries());
+
+        let witness_oracles: Vec<_> = ["a", "b", "c", "d", "e"]
+            .into_iter()
+            .map(|label| WitnessProverOracle::<F> {
+                label: label.to_string(),
+                poly: DensePolynomial::default(),
+                evals_at_coset_of_extended_domain: None,
+                queried_rotations: BTreeSet::new(),
+                should_mask: true,
+            })
+            .collect();
+
+        let instance_oracles = vec![];
+
+        let fixed_oracles: Vec<_> = ["q1", "q2", "q3", "q4", "q5"]
+            .into_iter()
+            .map(|label| FixedOracle::<F, PC> {
+                label: label.to_string(),
+                poly: DensePolynomial::default(),
+                evals_at_coset_of_extended_domain: None,
+                queried_rotations: BTreeSet::new(),
+                commitment: None,
+            })
+            .collect();
+
+        rescue_vo.configure(
+            &witness_oracles,
+            &instance_oracles,
+            &fixed_oracles,
+        );
     }
 }
