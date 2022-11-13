a33c6a8eb698663e0e7bdd5d1237027c74a5deaa
David Nevado <david.nevado@iohk.io>
2022-11-13 14:33:40 +0100

    Add missing evaluations errors to piop

--- src/piop/error.rs
+++ src/piop/error.rs
@@ -1,7 +1,16 @@
 #[derive(Debug, PartialEq)]
 pub enum Error {
-    MissingConcreteEval,
+    /// Missing evaluation at concrete oracle
+    MissingConcreteEval(String),
+    MissingExtendedEvals,
+    /// Missing witness oracle with givien label
+    MissingWtnsOracle(String),
+    /// Missing fixed oracle with givien label
+    MissingFixedOracle(String),
+    /// Missing instance oracle with givien label
+    MissingInstanceOracle(String),
+    /// Query index exceeds witness oracle size
     WtnsQueryIndexOutOfBounds(usize),
+    /// Query index exceeds instance oracle size
     InstanceQueryIndexOutOfBounds(usize),
-    ExtendedEvalsMissing,
 }
