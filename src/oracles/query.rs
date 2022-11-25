d20935461b354f8c5291f6580d0327573e79d194
David Nevado <david.nevado@iohk.io>
2022-11-25 18:25:04 +0100

    Remove unused `QueryContext`

--- src/oracles/query.rs
+++ src/oracles/query.rs
@@ -1,38 +1,5 @@
 use super::rotation::Rotation;
 use core::fmt;
-// use ark_ff::{Field, PrimeField};
-
-// pub type DomainSize = usize;
-// pub type OmegaI = usize;
-
-// pub enum QueryContext<F: Field> {
-//     Challenge(F),
-//     ExtendedCoset(DomainSize, Rotation, OmegaI),
-// }
-
-// impl<F: PrimeField> QueryContext<F> {
-//     pub fn replace_omega(&mut self, new_row: usize) {
-//         match self {
-//             Self::ExtendedCoset(_, _, old_w) => {
-//                 let _ = std::mem::replace(old_w, new_row);
-//             }
-//             Self::Challenge(_) => {
-//                 panic!("Can't replace omega in Challenge")
-//             }
-//         }
-//     }
-
-//     pub fn replace_rotation(&mut self, new_rot: Rotation) {
-//         match self {
-//             Self::ExtendedCoset(_, old_rotation, _) => {
-//                 let _ = std::mem::replace(old_rotation, new_rot);
-//             }
-//             Self::Challenge(_) => {
-//                 panic!("Can't replace rotation in Challenge")
-//             }
-//         }
-//     }
-// }
 
 #[derive(Clone)]
 pub enum OracleType {
