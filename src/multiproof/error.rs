2b0db11dc424576513ddf04369d9444d371ff05f
CPerezz <carlos.perezbaro@iohk.io>
2023-01-13 20:08:51 +0100

    remove: Unused error enum variants

--- src/multiproof/error.rs
+++ src/multiproof/error.rs
@@ -11,14 +11,6 @@ pub enum Error<E> {
     OpeningCheckFailed,
 }
 
-impl<E> Error<E> {
-    /// Convert an error in the underlying polynomial commitment scheme
-    /// to a `Error`.
-    pub fn from_pc_err(err: E) -> Self {
-        Self::PolynomialCommitmentError(err)
-    }
-}
-
 impl<E> From<PiopError> for Error<E> {
     fn from(err: PiopError) -> Self {
         Error::PIOPError(err)
